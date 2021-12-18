2c9db5fab1e9d6360e3ca1b8fec3d356acd74b17
GavinMasterson <gavinmasterson@gmail.com>
2021-12-18 21:22:15 +0000

    feat: Add script demonstrating shell construction of pipeline.

---
+++ pipeline_script.sh
@@ -0,0 +1,14 @@
+dvc run -n prepare \
+        -d data_prep.R \
+        -o data/penguins.csv \
+        Rscript data_prep.R
+
+dvc run -n analyse \
+        -d analysis.R -d data/penguins.csv \
+        -o data/outputs.RData \
+        Rscript analysis.R
+
+dvc run -n report \
+        -d report.R -d report_template.Rmd -d data/outputs.RData \
+        -o report.html \
+        Rscript report.R
