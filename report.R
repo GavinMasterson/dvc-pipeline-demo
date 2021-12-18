b3d1885e51ff8590c9c0f2aefd956f0400a1b9e7
GavinMasterson <gavinmasterson@gmail.com>
2021-12-18 21:54:06 +0000

    fix: Need pandoc for yaml front matter in Rmd report.

--- report.R
+++ report.R
@@ -2,8 +2,14 @@
 # Created by: Gavin Masterson 
 # 18 December 2021
 
-knitr::knit2html(
+# knitr::knit2html(
+#   input = "report_template.Rmd",
+#   output = "report.html",
+#   quiet = TRUE
+# )
+
+rmarkdown::render(
   input = "report_template.Rmd",
-  output = "report.html",
-  quiet = TRUE
+  output_format = "html_document",
+  output_file = "report.html",
 )
