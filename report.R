20c630b28a359c9066e9cf2dd0ada06ac558dc8d
GavinMasterson <gavinmasterson@gmail.com>
2021-12-18 21:32:31 +0000

    fix: Use correct pkg/function to generate the Rmarkdown report.

--- report.R
+++ report.R
@@ -1,5 +1,10 @@
-library(knitr)
-knitr::knit2html(
+#! usr/bin/env Rscript
+# Created by: Gavin Masterson 
+# 18 December 2021
+
+rmarkdown::render(
   input = "report_template.Rmd",
-  output = "report.html"
+  output_format = "html_document",
+  output_file = "report.html",
+  clean = TRUE
 )
