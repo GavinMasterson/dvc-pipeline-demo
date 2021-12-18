74e1a62a1fc377d55547e32df3312ba4d46e9089
GavinMasterson <gavinmasterson@gmail.com>
2021-12-18 21:40:53 +0000

    fix: Using rmarkdown creates pandoc issue. Use knitr instead.

--- report.R
+++ report.R
@@ -2,9 +2,8 @@
 # Created by: Gavin Masterson 
 # 18 December 2021
 
-rmarkdown::render(
+knitr::knit2html(
   input = "report_template.Rmd",
-  output_format = "html_document",
-  output_file = "report.html",
-  clean = TRUE
+  output = "report.html",
+  quiet = TRUE
 )
